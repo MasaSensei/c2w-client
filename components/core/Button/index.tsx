29f0781b33703af3894a7a4a0d35a815e362cada
Masayoshi <masasensei26@mail.com>
2025-01-31 14:13:54 +0700

    add: code page

--- components/core/Button/index.tsx
+++ components/core/Button/index.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button as ShadcnButton } from "@/components/ui/button";
 import { useRouter, usePathname } from "next/navigation";
@@ -12,6 +10,8 @@ type ButtonProps = {
   onClick?: () => void;
   className?: string;
   to?: string;
+  childrenSubmenu?: ReactNode;
+  isCollapsed?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({
@@ -21,22 +21,32 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
   className = "",
   to,
+  childrenSubmenu,
+  isCollapsed,
 }) => {
   const router = useRouter();
   const pathname = usePathname();
   const isActive = to && pathname === to;
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     if (onClick) onClick();
     if (to) router.push(to);
+
+    // Toggle submenu hanya untuk parent submenu
+    if (type === "submenuParent" && !isCollapsed) {
+      setIsOpen(!isOpen);
+    }
   };
 
-  // Konfigurasi styling berdasarkan tipe button
+  // Styling button berdasarkan tipe
   const typeStyles = {
     default:
       "text-slate-500 bg-transparent hover:bg-green-600 hover:text-white",
-    submenuParent: "text-slate-500 bg-transparent",
-    submenuChild: "text-slate-500 bg-transparent",
+    submenuParent:
+      "text-slate-500 bg-transparent hover:bg-green-600 hover:text-white",
+    submenuChild:
+      "text-slate-500 text-justify px-4 bg-transparent hover:bg-green-600 hover:text-white",
     filter:
       "border border-gray-400 text-gray-600 py-1.5 px-3 rounded-lg flex items-center gap-1 text-xs bg-white",
     add: "border border-white bg-green-500 text-white py-1.5 px-3 rounded-lg flex items-center gap-1 text-xs",
@@ -52,23 +62,37 @@ const Button: React.FC<ButtonProps> = ({
   );
 
   return (
-    <ShadcnButton onClick={handleClick} className={buttonClass}>
-      {icon && <span className="mr-2">{icon}</span>}
-      <span className="text-sm mt-[0.5px] flex-1">{children}</span>
-      {type === "submenuParent" && (
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          strokeWidth="0"
-          viewBox="0 0 448 512"
-          className="ml-2"
-          height="14"
-          width="14"
-        >
-          <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
-        </svg>
+    <>
+      <ShadcnButton onClick={handleClick} className={buttonClass}>
+        {icon && <span className="mr-2">{icon}</span>}
+        <span className="text-sm mt-[0.5px] text-justify flex-1">
+          {children}
+        </span>
+        {type === "submenuParent" && (
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 448 512"
+            className="ml-2"
+            height="14"
+            width="14"
+          >
+            <path
+              d={
+                isOpen
+                  ? "M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"
+                  : "M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+              }
+            ></path>
+          </svg>
+        )}
+      </ShadcnButton>
+      {/* Render submenu jika isOpen */}
+      {isOpen && childrenSubmenu && (
+        <ul className="ml-6 mt-2 text-start">{childrenSubmenu}</ul>
       )}
-    </ShadcnButton>
+    </>
   );
 };
 
--- components/layouts/Sidebar/index.tsx
+++ components/layouts/Sidebar/index.tsx
@@ -62,20 +62,60 @@ const Sidebar = () => {
               to="/suppliers"
               icon={
                 <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
                   viewBox="0 0 640 512"
                   className="mr-2"
                   height="1em"
                   width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M624 352h-16V243.9c0-12.7-5.1-24.9-14.1-33.9L494 110.1c-9-9-21.2-14.1-33.9-14.1H416V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h16c0 53 43 96 96 96s96-43 96-96h128c0 53 43 96 96 96s96-43 96-96h48c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM160 464c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm320 0c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm80-208H416V144h44.1l99.9 99.9V256z"></path>
                 </svg>
               }
             >
               {!isCollapsed && "Suppliers"}
+            </Cores.Button>
+          </li>
+          <hr />
+          <li className="my-2">
+            <Cores.Button
+              type="submenuParent"
+              icon={
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M4 6c0 1.657 3.582 3 8 3s8 -1.343 8 -3s-3.582 -3 -8 -3s-8 1.343 -8 3"></path>
+                  <path d="M4 6v6c0 1.657 3.582 3 8 3c.21 0 .42 -.003 .626 -.01"></path>
+                  <path d="M20 11.5v-5.5"></path>
+                  <path d="M4 12v6c0 1.657 3.582 3 8 3"></path>
+                  <path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+                  <path d="M19.001 15.5v1.5"></path>
+                  <path d="M19.001 21v1.5"></path>
+                  <path d="M22.032 17.25l-1.299 .75"></path>
+                  <path d="M17.27 20l-1.3 .75"></path>
+                  <path d="M15.97 17.25l1.3 .75"></path>
+                  <path d="M20.733 20l1.3 .75"></path>
+                </svg>
+              }
+              isCollapsed={isCollapsed}
+              childrenSubmenu={
+                <>
+                  <li className="mb-2">
+                    <Cores.Button type="submenuChild" to="/code">
+                      Kode
+                    </Cores.Button>
+                  </li>
+                </>
+              }
+            >
+              {!isCollapsed && "Master Data"}
             </Cores.Button>
           </li>
         </ul>
